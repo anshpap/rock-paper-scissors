75d5935f138421dee5a375aaef788fa6daeaf158
Anshul Papneja <anshulpapneja@outlook.com>
2022-07-08 12:00:00 -0400

    Add function that plays 5 rounds of the game, and add function to declare winner

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
     if (playerSelection === computerSelection) {        //Player and computer choose the same
         return 'draw';
     }
@@ -38,3 +36,40 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Enter your choice');
+        playerSelection = playerSelection.toLowerCase();
+        let computerSelection = computerPlay();
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === 'player wins') {
+            playerScore++;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        } else if (result === 'computer wins') {
+            computerScore++;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        } else {
+            console.log(`It's a draw! You both selected ${playerSelection}`);
+        }
+        console.log(`Your score: ${playerScore}`);
+        console.log(`Computer's score: ${computerScore}`);
+    }
+
+    declareWinner(playerScore, computerScore);
+}
+
+function declareWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        console.log('You won the game!');
+    } else if (computerScore > playerScore) {
+        console.log('You lost the game!');
+    } else {
+        console.log('It\'s a draw!');
+    }
+}
+
+game();
